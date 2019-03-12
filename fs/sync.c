cb7175992f8283a3ebb8be8184df2237666bc655
magchuz <magchuz@gmail.com>
2019-03-12 10:01:14 +0000

    Revert "tRy tO fIX"
    This reverts commit 8f2ffaf9bfbeff21f024ac5584e23b1610323b8c.

--- fs/sync.c
+++ fs/sync.c
@@ -17,12 +17,12 @@
 #include <linux/backing-dev.h>
 #include "internal.h"
 
+bool fsync_enabled = false;
+module_param(fsync_enabled, bool, 0755);
+
 #ifdef CONFIG_DYNAMIC_FSYNC
 #include <linux/dyn_sync_cntrl.h>
 #endif
-
-bool fsync_enabled = true;
-module_param(fsync_enabled, bool, 0755);
 
 #define VALID_FLAGS (SYNC_FILE_RANGE_WAIT_BEFORE|SYNC_FILE_RANGE_WRITE| \
 			SYNC_FILE_RANGE_WAIT_AFTER)
